90a90252e4ce8c52e53c21e46cfa772e49fb7b6b
MacMiniM1 <alonzo.choque@icloud.com>
2022-03-18 15:09:36 -0400

    MacMiniM1 20220318 15:09 First Commit and First Example

---
+++ SwiftUI-Simple-MAPKit-View/SwiftUI-Simple-MAPKit-View/SwiftUI_Simple_MAPKit_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Simple_MAPKit_ViewApp.swift
+//  SwiftUI-Simple-MAPKit-View
+//
+//  Created by Rene Alonzo Choque Saire on 18/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Simple_MAPKit_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
